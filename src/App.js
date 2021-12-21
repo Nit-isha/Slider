ce3097061fcfc376c64336c19da6e7a1b0a7a9d2
Nitisha <nitishagupta.1999@gmail.com>
2021-12-21 12:07:52 +0530

    feat: functionality add

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { FaQuoteRight } from "react-icons/fa";
-import data from "./data";
-
-export default function App() {
-  return <div></div>;
-}
